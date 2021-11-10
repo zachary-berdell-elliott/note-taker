03aab10739e24bf0f106324b7d38237944208dc8
zachary <zacharyberdell@gmail.com>
2021-11-09 17:00:14 -0700

    Server now loads the css and js.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+//Required modules
 const express = require("express");
 const path = require("path");
-
+//Variable declaration
 const app = express();
 const db = require("./db/db.json");
 const port = process.env.PORT || 3000;
@@ -12,16 +13,20 @@ function getReq(webDir, fileDir){
   });
 }
 
+app.use(express.static(__dirname + '/public'));
+
 //Sends index
 getReq('/', 'public/index.html');
 
 //Sends notes
 getReq('/notes', 'public/notes.html');
 
+//Sends the saved notes
 app.get('/api/notes', (req, res) => {
     res.json(db);
 });
 
+//Creates server
 app.listen(port, ()=> {
   console.log("The server is running.")
 })
